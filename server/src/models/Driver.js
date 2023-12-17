492886e4a4884ffedae1b3af87de724dc738d469
Boschaynamo <nico_947quil@hotmail.com>
2023-12-17 03:06:50 -0300

    Models added

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,12 +1,37 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Driver', {
+  sequelize.define("Driver", {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    apellido:{
+      type: DataTypes.TEXT,
+      allowNull:false,
+    },
+    descripcion:{
+      type: DataTypes.TEXT,
+      allowNull:false
+    },
+    imagen:{
+      type: DataTypes.TEXT,
+      allowNull:false
+    },
+    nacionalidad:{
+      type:DataTypes.TEXT,
+      allowNull:false
+    },
+    fechadenacimiento:{
+      type:DataTypes.DATE,
+      allowNull:false
+    }
   });
 };
