e9d3e61971beac7020aaf020efa9148da3fe04f8
Boschaynamo <nico_947quil@hotmail.com>
2024-01-03 00:05:43 -0300

    Added developer data

--- server/src/testTable.js
+++ server/src/testTable.js
@@ -1,6 +1,6 @@
-const { Driver,Team } = require("./db.js");
+const { Driver, Team } = require("./db.js");
 
-module.exports = async() => {
+module.exports = async () => {
   const namo = await Driver.create({
     name: "Namito",
     id: 2222,
@@ -10,26 +10,36 @@ module.exports = async() => {
     nacionalidad: "Argentina",
     fechadenacimiento: "1998-01-20 08:30:00",
   });
+  const namo2 = await Driver.create({
+    name: "Namito2",
+    id: 2223,
+    apellido: "Namardopolis2",
+    descripcion: "El mejor piloto de Formula 1 de la historia2",
+    imagen: "defaultimagenbro2",
+    nacionalidad: "Argentina",
+    fechadenacimiento: "1998-01-20 08:30:00",
+  });
   const mercedes = await Team.create({
     nombre: "Mercedes",
     id: 1,
-  })
+  });
   await Team.create({
     nombre: "Mercedes2",
     id: 2,
-  })
-  await Team.create({
+  });
+  const mercedes3 = await Team.create({
     nombre: "Mercedes3",
     id: 3,
-  })
+  });
   await Team.create({
     nombre: "Mercedes4",
     id: 4,
-  })
+  });
   const mercedes5 = await Team.create({
     nombre: "Mercedes5",
     id: 5,
-  })
+  });
   namo.addTeam(mercedes);
   namo.addTeam(mercedes5);
+  namo2.addTeam(mercedes3);
 };
