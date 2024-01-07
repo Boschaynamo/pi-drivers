4639618b204635eff76510081c501d324fc30c99
Boschaynamo <nico_947quil@hotmail.com>
2024-01-07 19:00:03 -0300

    Route POST| /drivers ok

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 
 const getDrivers = require("../controllers/getDrivers");
 const getDriverById = require("../controllers/getDriverById");
+const createDrivers = require("../controllers/createDrivers");
 
 const router = Router();
 //mis rutas
 
 router.get("/drivers/:idDriver", getDriverById);
 router.get("/drivers", getDrivers);
+router.post("/drivers", createDrivers);
 
 module.exports = router;
