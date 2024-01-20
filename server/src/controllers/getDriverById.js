7d3e309f1b68b62697f66e6f73f9d993e7dfdacc
Boschaynamo <nico_947quil@hotmail.com>
2024-01-20 15:52:50 -0300

    Fix detail route now handles ok the id

--- server/src/controllers/getDriverById.js
+++ server/src/controllers/getDriverById.js
@@ -9,14 +9,15 @@ module.exports = async (req, res) => {
     let selectedDriver = data.filter(
       (driver) => driver.id === Number(idDriver)
     );
-    if (!selectedDriver.length) {
+    if (selectedDriver.length===0) {
       selectedDriver = await Driver.findByPk(Number(idDriver), {
         include: Team,
       });
       if (selectedDriver === null)
         return res.status(404).json({ error: "Driver not found" });
-    }
     return res.status(200).json(formatDataToApi(selectedDriver.dataValues));
+    }
+    return res.status(200).json(selectedDriver[0])
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
