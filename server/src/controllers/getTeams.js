b5713ff8a171b80379efe0588b8eb2ccec6f4198
Boschaynamo <nico_947quil@hotmail.com>
2024-01-21 00:17:01 -0300

    Form component done

--- server/src/controllers/getTeams.js
+++ server/src/controllers/getTeams.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
         return driver.teams?.split(/,\s*/);
       })
       .flat();
-    const uniqueTeams = [...new Set(onlyTeams)].filter((team) => team);
+    const uniqueTeams = [...new Set(onlyTeams.map(team => team?.trim()))].filter((team) => team);
     //uniqueTeams has the teams clean and in an array: ["McLaren","Mercedes","Prost",...,"Team"]
 
     //Formatting for db
@@ -23,10 +23,11 @@ module.exports = async (req, res) => {
 
     for (let i = 0; i < focTeams.length; i++) {
       const [team, created] = await Team.findOrCreate(focTeams[i]);
-        console.log(team, created);
+        // console.log(team, created);
     }
 
-    return res.status(200).json(uniqueTeams);
+    const orderedTeams = uniqueTeams.sort((a,b)=> a.toLowerCase().localeCompare(b.toLowerCase()))
+    return res.status(200).json(orderedTeams);
   } catch (error) {
     return res.status(500).json({ error: error });
   }
