22616b86e3af58e1e1f701fe1eb2792021d7a8b0
Boschaynamo <nico_947quil@hotmail.com>
2024-01-24 01:08:35 -0300

    CSS Landing fix

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Landing = () => {
   return <div className={style.container}>
+    <h1 className={style.title}>Drivers application</h1>
     <NavLink to='/home'> <button className={style.button}>Go To Home</button> </NavLink>
     </div>;
 };
