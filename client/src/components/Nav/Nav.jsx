59cd0e6a5c4c551ad5f59e2f8ff89255c09a29ff
Boschaynamo <nico_947quil@hotmail.com>
2024-01-26 02:50:53 -0300

    CSS NavBar

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,11 +1,12 @@
 import SearchBar from "../SearchBar/SearchBar";
 import style from "./Nav.module.css"
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 const Nav = () =>{
+    const location = useLocation();
 
     return <div className={style.container}>
-        <NavLink className={style.botoncitos} to='/home'><span >Home</span></NavLink>
-        <NavLink className={style.botoncitos} to='/form'><span >Crear un Driver</span></NavLink>
+        <NavLink className={location.pathname === '/home' ? style.botoncitosActive : style.botoncitos} to='/home'><span >Home</span></NavLink>
+        <NavLink className={location.pathname === '/form' ? style.botoncitosActive : style.botoncitos} to='/form'><span >Crear un Driver</span></NavLink>
         <SearchBar />
     </div>
 }
